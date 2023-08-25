fb09005dd449d4aee9059ba9a3dfe6eb24c4d2af
sinofseven <em.s.00001@gmail.com>
2023-08-25 17:33:04 +0900

    impl

---
+++ mian.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "remote" {
+    organization = "cm-natsume"
+    workspaces {
+      name = "terraform-cloud-version-test"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_sns_topic" "tmp_001" {}
